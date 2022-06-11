ccc58f750f8cb0f10645d34263583cb44bd17154
userakf <820279897@qq.com>
2022-06-12 00:35:11 +0800

    home

---
+++ src/components/header/index.vue
@@ -0,0 +1,101 @@
+<template>
+    <div id="header">
+        <header>
+            <div class="icon search">
+                <Icon size='20' type='search' />
+            </div>
+            <div class="text">
+                <h3>Management Task</h3>
+                <p>We make your projects happen!</p>
+            </div>
+            <div class="icon user">
+                <Icon size="20" type="success" />
+            </div>
+        </header>
+        <div class="slide">
+            <div class="item todo active">
+                <span>To do</span>
+            </div>
+            <div class="item progress">
+                <span>In progress</span>
+            </div>
+            <div class="item completed">
+                <span>Completed</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@tarojs/components';
+</script>
+
+<style scoped lang="scss">
+::-webkit-scrollbar{
+    opacity: 0;
+}
+#header{
+    background-color: #F6F9FF;
+    border-bottom-left-radius: 60px;
+    border-bottom-right-radius: 60px;
+    padding-bottom: 20px;
+    header{
+        padding: 50px 40px;
+        display: flex;
+        justify-content: space-between;
+        align-items:flex-start;
+    }
+    .icon{
+        height: auto;
+        display: inline-block;
+        width: 88px;
+        height: 88px;
+        background-color: #FFFFFF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 15px;
+    }
+    
+    .search{
+
+    }
+    .text{
+        text-align: center;
+        h3{
+            font-size: 40px;
+        }
+        p{
+            font-size: 13px;
+        }
+    }
+    .user{}
+
+    .slide{
+        width: 100%;
+        margin: 30px 40px;
+        overflow-y: scroll; 
+        white-space: nowrap;
+        
+        .item{
+            width: auto;
+            height: 84px;
+            margin-right: 30px;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 15px;
+            background-color: #FFFFFF;
+            padding: 0 80px;
+            &.active{
+                background-color: #3E7FFF;
+                color: #FFFFFF;
+            }
+        }
+        span{
+            font-size: 15px;
+        }
+    }
+}
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,25 @@
 <template>
-  <view class="index">
-    <text>{{ msg }}</text>
-  </view>
+  <Header />
+  <div class="conatiner">
+    <div class="header">
+      <div class="text">
+        <p>Excellence management</p>
+        <span>Lorem ipsum dolor sit amet</span>
+      </div>
+      <div class="icon">
+        <Icon size="20" type="info"/>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script lang="ts" setup>
 import './index.scss'
-
-export default {
-  setup () {
-    const msg = ref('Hello world')
-    return {
-      msg
-    }
-  }
-}
+import Header from '../../components/header/index.vue'
+import { Icon } from '@tarojs/components';
 </script>
+<style lang="scss">
+.conatiner{
+  padding: 30px 60px;
+}
+</style>
